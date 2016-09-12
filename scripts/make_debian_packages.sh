29473f2ca44f9a737c59b7b063364cfed189e3e3
Jean Marc LACROIX <jeanmarc.lacroix@free.fr>
2016-09-12 19:22:55 +0200

    make_debian_package: manage user choice done during make menuconfig choices

--- scripts/make_debian_packages.sh
+++ scripts/make_debian_packages.sh
@@ -1,21 +1,22 @@
-#! /usr/bin/env sh
+#!/bin/bash
 #
-#
-# Copyright (c) 2015-2016, Jean Guyomarc'h <jean.guyomarch@gmail.com>
-#
+
+# Copyright  (c) 2015-2016, Jean Guyomarc'h <jean.guyomarch@gmail.com>
+# Copyright (c) 2016, Jean Mar LACROIX <jeanmarc.lacroi@free.fr>
+
 # This file is part of SBXG
-#
-# SBXG is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+
+# SBXG is  free software: you  can  redistribute  it and/or modify  it
+# under  the terms of the GNU  General  Public License as published by
+# the  Free Software Foundation,  either version 3  of the License, or
 # (at your option) any later version.
-#
-# SBXG is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
+
+# SBXG is distributed in the hope that it will  be useful, but WITHOUT
+# ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY
+# or FITNESS FOR  A PARTICULAR PURPOSE.  See   the GNU General  Public
+# License for more details.
+
+# You should  have received a copy of  the GNU  General Public License
 # along with SBXG.  If not, see <http://www.gnu.org/licenses/>.
 
 set -e
@@ -24,17 +25,10 @@ set -x
 # Source the main configuration
 . "$(dirname "$0")"/config
 
-# Debian packages to generate with make-kpkg
-DEB_PACKAGES="
-kernel_image
-kernel_headers
-kernel_doc
-kernel_source
-"
 
-#==============================================================================#
-#                          Create the debian packages                          #
-#==============================================================================#
+#====================================================================#
+# Create               the               debian    packages          #
+#====================================================================#
 
 # SHA1 of build tool
 SHA1_BASE=$(git rev-parse --short HEAD)
@@ -48,15 +42,67 @@ make ARCH=arm include/config/kernel.release
 SHA1_KERNEL=$(git rev-parse --short HEAD)
 KERNEL_VERSION="$(cat "include/config/kernel.release" | cut -f 1 -d '-')"
 
-DISABLE_PAX_PLUGINS=y \
-LOADADDR="$CONFIG_LOADADDR" \
-DEB_HOST_ARCH="$CONFIG_ARCH" \
-make-kpkg --rootcmd fakeroot \
-          --revision "${KERNEL_VERSION}+${SHA1_BASE}~${SHA1_KERNEL}" \
-          --uimage \
-          --append_to_version "-${SHA1_BASE}-${SHA1_KERNEL}" \
-          --jobs "$CONFIG_JOBS" \
-          --initrd \
-          --arch arm \
-          --cross-compile "$CONFIG_GCC_PREFIX" \
-          $DEB_PACKAGES
+# According User choice  (when launching  make menuconfig in  topdir),
+# generate correct Debian package
+
+if [ x$CONFIG_MAKE_KPKG_KERNEL_IMAGE == xy ]; then
+	DISABLE_PAX_PLUGINS=y \
+		LOADADDR="$CONFIG_LOADADDR" \
+		DEB_HOST_ARCH="$CONFIG_ARCH" \
+		make-kpkg --rootcmd fakeroot \
+		--revision "${KERNEL_VERSION}+${SHA1_BASE}~${SHA1_KERNEL}" \
+		--uimage \
+		--append_to_version "-${SHA1_BASE}-${SHA1_KERNEL}" \
+		--jobs "$CONFIG_JOBS" \
+		--initrd \
+		--arch arm \
+		--cross-compile "$CONFIG_GCC_PREFIX" \
+		kernel_image	
+fi
+
+if [ x$CONFIG_MAKE_KPKG_KERNEL_HEADERS == xy ]; then
+	DISABLE_PAX_PLUGINS=y \
+		LOADADDR="$CONFIG_LOADADDR" \
+		DEB_HOST_ARCH="$CONFIG_ARCH" \
+		make-kpkg --rootcmd fakeroot \
+		--revision "${KERNEL_VERSION}+${SHA1_BASE}~${SHA1_KERNEL}" \
+		--uimage \
+		--append_to_version "-${SHA1_BASE}-${SHA1_KERNEL}" \
+		--jobs "$CONFIG_JOBS" \
+		--initrd \
+		--arch arm \
+		--cross-compile "$CONFIG_GCC_PREFIX" \
+		kernel_headers	
+fi
+
+if [ x"$CONFIG_MAKE_KPKG_KERNEL_DOC" == "xy" ]; then
+	DISABLE_PAX_PLUGINS=y \
+		LOADADDR="$CONFIG_LOADADDR" \
+		DEB_HOST_ARCH="$CONFIG_ARCH" \
+		make-kpkg --rootcmd fakeroot \
+		--revision "${KERNEL_VERSION}+${SHA1_BASE}~${SHA1_KERNEL}" \
+		--uimage \
+		--append_to_version "-${SHA1_BASE}-${SHA1_KERNEL}" \
+		--jobs "$CONFIG_JOBS" \
+		--initrd \
+		--arch arm \
+		--cross-compile "$CONFIG_GCC_PREFIX" \
+		kernel_doc	
+fi
+
+if [ x"$CONFIG_MAKE_KPKG_KERNEL_SOURCE" == "xy" ]; then
+	DISABLE_PAX_PLUGINS=y \
+		LOADADDR="$CONFIG_LOADADDR" \
+		DEB_HOST_ARCH="$CONFIG_ARCH" \
+		make-kpkg --rootcmd fakeroot \
+		--revision "${KERNEL_VERSION}+${SHA1_BASE}~${SHA1_KERNEL}" \
+		--uimage \
+		--append_to_version "-${SHA1_BASE}-${SHA1_KERNEL}" \
+		--jobs "$CONFIG_JOBS" \
+		--initrd \
+		--arch arm \
+		--cross-compile "$CONFIG_GCC_PREFIX" \
+		kernel_source	
+fi
+
+exit 0
